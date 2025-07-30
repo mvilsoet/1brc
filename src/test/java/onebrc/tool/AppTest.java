a138133039fb9b90ae6aeb9d77b7b8e4062d9b69
Michael Vilsoet <57650580+mvilsoet@users.noreply.github.com>
2025-07-30 12:56:15 -0400

    Working test at 1b rows

--- src/test/java/onebrc/tool/AppTest.java
+++ src/test/java/onebrc/tool/AppTest.java
@@ -7,6 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 public class AppTest {
     @Test
     public void testPipeline() {
-        assertDoesNotThrow(() -> App.main(new String[] {"src/test/resources/measurements10M.txt"}));
+        assertDoesNotThrow(() -> App.main(new String[] {"src/test/resources/measurements.txt"}));
     }
 }
